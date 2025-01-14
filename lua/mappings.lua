585e40aa1b7bbae3919d2cb585c311f95d10039a
Kartikey Pandey <kartikey224252@gmail.com>
2025-01-14 23:13:24 +0530

    some allignment fix

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,26 +14,6 @@ map("n", "k", "gk", { desc = "Move up by visual line" })
 map("v", "j", "gj", { desc = "Move down by visual line" })
 map("v", "k", "gk", { desc = "Move up by visual line" })
 
--- DAP mappings
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = " DAP Add breakpoint at line" })
-map("n", "<leader>dus", function()
-  local widgets = require "dap.ui.widgets"
-  local sidebar = widgets.sidebar(widgets.scopes)
-  sidebar.open()
-end, { desc = "DAP Open debugging sidebar" })
-
--- DAP Go mappings
-map("n", "<leader>dgt", function()
-  require("dap-go").debug_test()
-end, { desc = "DAP Debug go test" })
-map("n", "<leader>dgl", function()
-  require("dap-go").debug_last()
-end, { desc = "DAP Debug last go test" })
-
--- Gopher mappings
-map("n", "<leader>gsj", "<cmd> GoTagAdd json <CR>", { desc = "Gopher Add json struct tags" })
-map("n", "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", { desc = "Gopher Add yaml struct tags" })
-
 -- Persistence mappings
 map("n", "<leader>qs", function()
   require("persistence").load()
